7901c68173d4759872fd5020e8514df8da2517bd
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-18 19:11:40 +0100

    Add basic video tests

--- videos/tests.py
+++ videos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Video
+
+
+class VideoModelTestCase(TestCase):
+    def setUp(self):
+        Video.objects.create(title='test title')
+
+    def test_valid_title(self):
+        title = 'test title'
+        qs = Video.objects.filter(title=title)
+        self.assertTrue(qs.exists())
+
+    def test_created_count(self):
+        title = 'test title'
+        qs = Video.objects.filter(title=title)
+        self.assertEqual(qs.count(), 1)
