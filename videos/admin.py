230a658537d39a98026ab12b366481e97273ea88
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-18 19:40:25 +0100

    Created proxy models and used them in admin

--- videos/admin.py
+++ videos/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
-from .models import Video, VideoProxy
+from .models import VideoAllProxy, VideoPublishedProxy
 
-admin.site.register(Video)
-admin.site.register(VideoProxy)
+
+class VideoAllProxyAdmin(admin.ModelAdmin):
+    list_display = ["title", "video_embed_id"]
+    search_fields = ["title"]
+
+    class Meta:
+        model = VideoAllProxy
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ["title", "video_embed_id"]
+    search_fields = ["title"]
+
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(active=True)
+
+
+admin.site.register(VideoAllProxy, VideoAllProxyAdmin)
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
--- videos/models.py
+++ videos/models.py
@@ -6,10 +6,18 @@ class Video(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video_embed_id = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
 
 
-class VideoProxy(Video):
+class VideoPublishedProxy(Video):
     class Meta:
         proxy = True
         verbose_name = "Published Video"
         verbose_name_plural = "Published Videos"
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = "Video"
+        verbose_name_plural = "All Videos"
