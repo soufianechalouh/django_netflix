f25ae5845942ee5dab7f9ee759cd532160d43733
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-19 22:13:39 +0100

    Feature: Add playlists app

--- django_netflix/settings.py
+++ django_netflix/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'videos'
+    'videos',
+    'playlists'
 ]
 
 MIDDLEWARE = [
