9d9d428fccdeb9f95a641b88b917cb1d302a3ef6
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-19 22:43:22 +0100

    Feature: Add playlist model

--- playlists/models.py
+++ playlists/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.utils import timezone
 
-# Create your models here.
+from django_netflix.db.models import PublishStateOptions
+from django_netflix.db.receivers import publish_state_pre_save, slugify_pre_save
+
+
+class PlaylistQueryset(models.QuerySet):
+    def published(self):
+        return self.filter(state=PublishStateOptions.PUBLISHED, publish_timestamp__lte=timezone.now())
+
+
+class PlaylistManager(models.Manager):
+    def get_queryset(self):
+        return PlaylistQueryset(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=230)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    state = models.CharField(max_length=2, choices=PublishStateOptions.choices, default=PublishStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = PlaylistManager()
+
+    @property
+    def is_published(self):
+        return self.active
+
+
+pre_save.connect(publish_state_pre_save, sender=Playlist)
+pre_save.connect(slugify_pre_save, sender=Playlist)
--- playlists/tests.py
+++ playlists/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.utils.text import slugify
 
-# Create your tests here.
+from django_netflix.db.models import PublishStateOptions
+from .models import Playlist
+
+
+class PlaylistModelTestCase(TestCase):
+    def setUp(self):
+        self.obj_a = Playlist.objects.create(title='test title')
+        self.obj_b = Playlist.objects.create(title='test published title', state=PublishStateOptions.PUBLISHED)
+
+    def test_valid_title(self):
+        title = 'test title'
+        qs = Playlist.objects.filter(title=title)
+        self.assertTrue(qs.exists())
+
+    def test_slug_field(self):
+        title = self.obj_a.title
+        slug = slugify(title)
+        self.assertEqual(self.obj_a.slug, slug)
+
+    def test_created_count(self):
+        title = 'test title'
+        qs = Playlist.objects.filter(title=title)
+        self.assertEqual(qs.count(), 1)
+
+    def test_draft_case(self):
+        qs = Playlist.objects.filter(state=PublishStateOptions.DRAFT)
+        self.assertEqual(qs.count(), 1)
+
+    def test_published_case(self):
+        now = timezone.now()
+        published_qs = Playlist.objects.filter(state=PublishStateOptions.PUBLISHED, publish_timestamp__lte=now)
+        self.assertTrue(published_qs.exists())
+
+    def test_publish_manager(self):
+        published_qs = Playlist.objects.all().published()
+        published_qs_2 = Playlist.objects.published()
+        self.assertTrue(published_qs.exists())
+        self.assertEqual(published_qs.count(), published_qs_2.count())
