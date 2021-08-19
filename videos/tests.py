fdfb3f5976f021fa00239323c92f6f5155e77268
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-19 18:01:25 +0100

    Test: Added test for state and published_timestamp

--- videos/tests.py
+++ videos/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils import timezone
 
 from .models import Video
 
@@ -6,6 +7,7 @@ from .models import Video
 class VideoModelTestCase(TestCase):
     def setUp(self):
         Video.objects.create(title='test title')
+        Video.objects.create(title='test published title', state=Video.VideoStateOptions.PUBLISHED)
 
     def test_valid_title(self):
         title = 'test title'
@@ -15,4 +17,13 @@ class VideoModelTestCase(TestCase):
     def test_created_count(self):
         title = 'test title'
         qs = Video.objects.filter(title=title)
+        self.assertEqual(qs.count(), 2)
+
+    def test_draft_case(self):
+        qs = Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
         self.assertEqual(qs.count(), 1)
+
+    def test_published_case(self):
+        now = timezone.now()
+        published_qs = Video.objects.filter(state=Video.VideoStateOptions.PUBLISHED, publish_timestamp__lte=now)
+        self.assertTrue(published_qs.exists())
