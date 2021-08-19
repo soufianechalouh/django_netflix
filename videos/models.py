b3847d95a88b430df877e5dc27dd4f63f2fcca92
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-19 17:50:03 +0100

    feature: Added publishing timestamp

--- videos/models.py
+++ videos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Video(models.Model):
@@ -14,10 +15,19 @@ class Video(models.Model):
     video_embed_id = models.CharField(max_length=255)
     active = models.BooleanField(default=True)
     state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
 
     @property
     def is_published(self):
         return self.active
+
+    def save(self, *args, **kwargs):
+        if self.state == self.VideoStateOptions.PUBLISHED and self.publish_timestamp is None:
+            print("save as timestamp for published")
+            self.publish_timestamp = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp = None
+        super().save(*args, **kwargs)
 
 
 class VideoPublishedProxy(Video):
