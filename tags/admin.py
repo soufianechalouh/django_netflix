0f81f5f27d6d10fc7beb2b33cfa6da395fe5c758
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-21 22:22:12 +0100

    Feature: created tag model and added it to admin

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from tags.models import TaggedItem
+
+
+class TaggedItemAdmin(admin.ModelAdmin):
+    fields = ["tag", "content_type", "object_id", "content_object"]
+    readonly_fields = ["content_object"]
+
+    class Meta:
+        model = TaggedItem
+
+
+admin.site.register(TaggedItem, TaggedItemAdmin)
