cc23a954279b9aec7e41cfedf2b848bcae0d8cad
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-08-27 22:27:05 +0100

    Feature: Add title to context using Mixin

--- playlists/views.py
+++ playlists/views.py
@@ -3,11 +3,22 @@ from django.views.generic import ListView
 from .models import MovieProxy, TVShowProxy
 
 
-class MovieListView(ListView):
+class PlaylistMixin():
+    title = None
     template_name = "playlist_list.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        if self.title is not None:
+            context["title"] = self.title
+        return context
+
+
+class MovieListView(PlaylistMixin, ListView):
     queryset = MovieProxy.objects.all()
+    title = "Movies"
 
 
-class TVShowView(ListView):
-    template_name = "playlist_list.html"
+class TVShowView(PlaylistMixin, ListView):
     queryset = TVShowProxy.objects.all()
+    title = "TV Shows"
