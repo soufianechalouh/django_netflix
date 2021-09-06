6a072bdfc187d8ed1eedfd6e4a6ed81b11c38108
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-09-06 09:35:47 +0100

    Feature: Added rating and rating average

--- ratings/models.py
+++ ratings/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
+from django.db.models import Avg
 
 User = settings.AUTH_USER_MODEL
 
@@ -16,9 +17,21 @@ class RatingChoices(models.IntegerChoices):
     __empty__ = "Unknown"
 
 
+class RatingQuerySet(models.QuerySet):
+    def rating(self):
+        return self.aggregate(average=Avg("value"))["average"]
+
+
+class RatingManager(models.Manager):
+    def get_queryset(self):
+        return RatingQuerySet(self.model, using=self._db)
+
+
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     value = models.IntegerField(null=True, blank=True, choices=RatingChoices.choices)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
+
+    objects = RatingManager()
